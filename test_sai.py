aa4c352e2b3340499491ff1b70704d6aac02d75e
Bhaswati-Sarma <Bhaswati.Sarma@infobellit.com>
2025-05-23 01:46:30 +0000

    added the function Zelda

--- test_sai.py
+++ test_sai.py
@@ -1,2 +1,14 @@
 print("Hello !")
 print("This is my new feature code")
+
+def Zelda():
+    n = input("You are in the Lost Forest\n****************\n****************\n :)\n****************\n****************\nGo left or right? (l/r)")
+    i = 0
+    while n == "right" or n == "Right" or n == 'r' or n == 'R':
+        i += 1
+        if i < 3:
+            face = ':)'
+        else:
+            face = ':|'
+        n = input("You are in the Lost Forest\n****************\n****************\n "+face+"\n****************\n****************\nGo left or right? (l/r)")
+    print("\nYou got out of the Lost Forest!\n\o/")
